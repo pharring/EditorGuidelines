f85efb1c7e00fb441099af92feaf850d40a0b358
Sam Harwell <sam@tunnelvisionlabs.com>
2019-01-31 09:18:58 -0600

    Convert ColumnGuide to an SDK project

--- src/ColumnGuide/Properties/AssemblyInfo.cs
+++ src/ColumnGuide/Properties/AssemblyInfo.cs
@@ -1,18 +1,11 @@
 ﻿// Copyright (c) Paul Harrington.  All Rights Reserved.  Licensed under the MIT License.  See LICENSE in the project root for license information.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ColumnGuide")]
-[assembly: AssemblyDescription("Editor Guidelines Extension")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Paul Harrington")]
-[assembly: AssemblyProduct("ColumnGuide")]
-[assembly: AssemblyCopyright("Copyright © 2018 Paul Harrington")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +13,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
